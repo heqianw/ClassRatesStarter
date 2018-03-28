51cbe5c8974de7fde0d8e781ecf2b92cd8a5a4eb
He Qian Wang <heqianw@gmail.com>
2018-03-28 19:27:33 -0400

    Got it

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -2,6 +2,37 @@
 // Retrieve API Key - https://azure.microsoft.com/en-ca/try/cognitive-services/
 // API Documentation - https://westus.dev.cognitive.microsoft.com/docs/services/TextAnalytics.V2.0/operations/56f30ceeeda5650db055a3c9
 function submitComment(commentControl) {
+	var comments = document.getElementsByName(commentControl)[0].value;
+	var url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
+	var apiKey = "57d17537689e439592b7f1212a0821cc";
+
+	var body = {
+		  "documents": [
+		    {
+		      "language": "en-US",
+		      "id": "1",
+		      "text": comments
+		    }
+		  ]
+		}
+		//http call, using this format
+		$.ajax({
+			type: "POST",
+			url: url,
+			data: JSON.stringify(body), 
+			processData: false,
+			headers: {
+				"Ocp-Apim-Subscription-Key": apiKey,
+				"Content-Type": "application/json"
+			}
+		}).done(function(result){
+			var sentimentRating = Math.round((result.documents[0].score * 100)/ 25) + 1;
+			var currentItemId = JSON.parse(localStorage.getItem('currentItemId'));
+			window.location.href = '/comments?classId=' + currentItemId + '&rating=' + sentimentRating + '&comments=' + comments;
+		}).fail(function(xhr, status, err){
+			alert(err);
+		});
+
     // TODO - Call API
 }
 
